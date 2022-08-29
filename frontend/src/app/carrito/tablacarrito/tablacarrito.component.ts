31ae945dc0321a02745a69675da505f1961afc17
ricdasal <ricdasal@espol.edu.ec>
2022-08-29 02:05:26 -0500

    agregando parte del frontend

--- frontend/src/app/carrito/tablacarrito/tablacarrito.component.ts
+++ frontend/src/app/carrito/tablacarrito/tablacarrito.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tablacarrito.component.css']
 })
 export class TablacarritoComponent implements OnInit {
+  listas:Array<any> = [];
+  lista_items:Array<any> = [];
+  mostrarTabla:boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    fetch("http://localhost:3000/pedidos")
+    .then(response => response.json())
+    .then(listas => {
+      this.listas = listas 
+    })
+
   }
+
+  obtenerItems(id_pedido:string){
+    this.mostrarTabla = false;
+    fetch("http://localhost:3000/item_pedidos/" + id_pedido)
+    .then(response => response.json())
+    .then(listas => {
+      this.lista_items = listas; 
+    })
+    for(let item of this.lista_items){
+      fetch("http://localhost:3000/productos/" + item.id_producto)
+      .then(response => response.json())
+      .then(listas => {
+        item.id_producto = listas[0].nombre;
+      })
+    } 
+  }
+
+
+
+
+  
 
 }
